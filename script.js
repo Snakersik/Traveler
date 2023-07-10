126e7ec7e0784badc7cd4507bf6ba6c4957f2803
Snakersik <konradtratwal@gmail.com>
2023-07-10 21:34:45 +0200

    Section2

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+const scrollToSection2 = () =>{
+    document.querySelector('#section--1').scrollIntoView({behavior: 'smooth'})
+
+}
+
+function scrollToHome(){
+    document.querySelector('.main').scrollIntoView({behavior: 'smooth'})
+}
